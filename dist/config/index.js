bb5e6f0778ddf2fd50124a7b22a97901a3314676
Riccorichards <richard.tropimovi.1@iliauni.edu.ge>
2023-11-17 13:23:50 +0400

    build the changes

--- dist/config/index.js
+++ dist/config/index.js
@@ -1,10 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.PORT = exports.JWT_PRIVATE_KEY = exports.SALT_NUM = exports.MONGO_URI = void 0;
+exports.PORT = exports.SALT_NUM = exports.MONGO_URI = void 0;
 exports.MONGO_URI = "mongodb://127.0.0.1:27017/Online_food_delivery";
-//export const MONGO_URI =
-//  "mongodb+srv://trriccorichards:OJdUYrvqsu29Nay4@onlinefoodtutorial.rf3berm.mongodb.net/onlineFoodTutorial?retryWrites=true&w=majority";
 exports.SALT_NUM = 13;
-exports.JWT_PRIVATE_KEY = "comeonbaby";
 exports.PORT = process.env.PORT || 8000;
 //# sourceMappingURL=index.js.map
--- dist/services/ExpressApp.js
+++ dist/services/ExpressApp.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -11,8 +20,12 @@ exports.default = (app) => {
     app.use(express_1.default.json());
     app.use(express_1.default.urlencoded({ extended: true }));
     app.use((0, cors_1.default)({
-        origin: "https://demo-online-delivery.vercel.app",
+        origin: "https://demo-online-food-delivery-client.vercel.app",
         credentials: true,
+        methods: ["post", "get", "put", "delete"],
+    }));
+    app.get("/check-healthy", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+        return res.status(200).json("Hello");
     }));
     app.use("/admin", routes_1.AdminRouter);
     app.use("/vendor", routes_1.VendorRoute);
