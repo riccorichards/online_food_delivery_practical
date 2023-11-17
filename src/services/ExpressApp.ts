6d00d4d550d0b7594f7b2c09345c087fff1be98a
Riccorichards <richard.tropimovi.1@iliauni.edu.ge>
2023-11-17 09:25:44 +0400

    update cors origin

--- src/services/ExpressApp.ts
+++ src/services/ExpressApp.ts
@@ -13,7 +13,7 @@ export default (app: Application) => {
   app.use(express.urlencoded({ extended: true }));
   app.use(
     cors({
-      origin: "http://localhost:3000",
+      origin: "https://demo-online-delivery.vercel.app",
       credentials: true,
     })
   );
